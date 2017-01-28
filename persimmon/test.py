ca53fb217bf04a073a942bedad5331f4a500716c
AlvarBer <alvaro.garcia95@hotmail.com>
2017-01-28 23:42:20 +0000

    Refactored code, now using properties properly

--- persimmon/test.py
+++ persimmon/test.py
@@ -5,6 +5,7 @@ import kivy
 kivy.require('1.9.0')
 import pandas as pd
 import backend
+from functools import partial, partialmethod
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -20,7 +21,6 @@ class TestApp(App):
         self.predict_data = None
 
     def validate(self):
-        print('Train file: {} |  Estimator: {}  | CV: {}'.format(self.train_file, self.estimator, self.cv))
         result = backend.perform(pd.read_csv(self.train_file, header=0),
                                  self.estimator, self.cv, predict_data=None)
         return '{:.3f}% (+/-{:.3f}%)'.format(result.mean() * 100,
@@ -34,43 +34,35 @@ class TestApp(App):
 class PrototypeScreen(BoxLayout):
     """ PrototypeScreen is a pumped box layout used for the first iteration """
     result = StringProperty()
-    selected_train_file = ObjectProperty()
+    text_input_train_file = StringProperty()
 
-    def load_popup(self):
-        content = LoadDialog(load_func=self.load_file,
-                             cancel_func=self.dismiss_popup)
-        self._popup = Popup(title='Load file', content=content,
-                            size_hint=(0.8, 0.8))
-        self._popup.open()
+    def load_popup(self, tinput):
+        content = LoadDialog(size_hint=(0.8, 0.8))
+        content.bind(selected_file=partial(self.callback, text_input=tinput))
+        content.open()
 
-    def dismiss_popup(self):
-        self._popup.dismiss()
+    def callback(self, instance, value, text_input):
+        text_input.text = value
 
-    def load_file(self, path, selection):
-        if selection:
-            App.get_running_app().train_file = selection[0]
-            self._popup.dismiss()
-            self.selected_train_file.text = selection[0]
+    def on_text_input_train_file(self, instance, value):
+        App.get_running_app().train_file = value
+        print(App.get_running_app().train_file)
 
     def validate(self):
         self.result = App.get_running_app().validate()
 
 
-class LoadDialog(FloatLayout):
-    cancel_func = ObjectProperty(None)
-    load_func = ObjectProperty(None)
+class LoadDialog(Popup):
     load_button = ObjectProperty(None)
-    selected_file = ObjectProperty(None)
+    selected_file = StringProperty(None)
 
-    def file_selected(self, selection):
+    def toggle_load_button(self, selection):
         if selection:
             filename = selection[0]
             self.load_button.disabled = False
         else:
             filename = ''
             self.load_button.disabled = True
-
-        self.selected_file.text = filename
 
 
 if __name__ == '__main__':
