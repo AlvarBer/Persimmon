f326ce2e34eab6bdaf9eeba0687609d6e8fd7ba5
AlvarBer <alvaro.garcia95@hotmail.com>
2017-01-29 00:13:59 +0000

    Removed unnecesary code

--- persimmon/test.py
+++ persimmon/test.py
@@ -5,7 +5,7 @@ import kivy
 kivy.require('1.9.0')
 import pandas as pd
 import backend
-from functools import partial, partialmethod
+from functools import partial
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -32,36 +32,33 @@ class TestApp(App):
 
 
 class PrototypeScreen(BoxLayout):
-    """ PrototypeScreen is a pumped box layout used for the first iteration """
+    """PrototypeScreen is a pumped box layout used for the first iteration."""
     result = StringProperty()
     text_input_train_file = StringProperty()
 
     def load_popup(self, tinput):
-        content = LoadDialog(size_hint=(0.8, 0.8))
-        content.bind(selected_file=partial(self.callback, text_input=tinput))
+        content = FileDialog(size_hint=(0.8, 0.8))
+        content.bind(selected_file=partial(self.change_file, text_input=tinput))
         content.open()
 
-    def callback(self, instance, value, text_input):
-        text_input.text = value
+    def change_file(self, instance, value, text_input):
+        self.text_input_train_file = value
 
     def on_text_input_train_file(self, instance, value):
         App.get_running_app().train_file = value
-        print(App.get_running_app().train_file)
 
     def validate(self):
         self.result = App.get_running_app().validate()
 
 
-class LoadDialog(Popup):
-    load_button = ObjectProperty(None)
-    selected_file = StringProperty(None)
+class FileDialog(Popup):
+    load_button = ObjectProperty()
+    selected_file = StringProperty()
 
     def toggle_load_button(self, selection):
         if selection:
-            filename = selection[0]
             self.load_button.disabled = False
         else:
-            filename = ''
             self.load_button.disabled = True
 
 
