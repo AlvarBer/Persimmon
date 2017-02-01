7a5207c52064a5836686f8529d89b22f6fb75eaa
AlvarBer <alvaro.garcia95@hotmail.com>
2017-02-01 00:03:33 +0000

    Added tabbed view and moved folders

---
+++ persimmon/view/view.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.behaviors import DragBehavior
+from kivy.uix.label import Label
+
+
+class ViewApp(App):
+    pass
+
+if __name__ == '__main__':
+    ViewApp().run()
