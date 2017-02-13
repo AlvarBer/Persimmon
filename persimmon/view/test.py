146a8e7dfe0a0618030bcf88653068cbf8b8f353
AlvarBer <alvaro.garcia95@hotmail.com>
2017-02-13 21:08:15 +0000

    Started work on blocks

--- persimmon/view/test.py
+++ persimmon/view/test.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-import os
 import kivy
 kivy.require('1.9.0')
 import pandas as pd
--- persimmon/view/view.py
+++ persimmon/view/view.py
@@ -2,10 +2,23 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.behaviors import DragBehavior
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty
+from kivy.uix.widget import Widget
 
 
 class ViewApp(App):
     pass
+
+       
+class Test(Widget):
+    test_property = StringProperty()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print('>>>>>>>>>>>')
+        print(self.test_property)
+        print('<<<<<<<<<<<')
 
 if __name__ == '__main__':
     ViewApp().run()
