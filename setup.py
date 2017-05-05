01098962c0cce47513b45ffbbcf09e0a95d35c45
AlvarBer <alvaro.garcia95@hotmail.com>
2017-05-05 21:24:44 +0100

    Pypi ready now

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(name='persimmon',
-      version='0.7',
       description='A visual dataflow language for sklearn',
-      url='http://github.com/alvarber/Persimmon',
       author='Álvaro Bermejo',
       author_email='alvaro.garcia95@hotmail.com',
+      version='0.8',
+      url='http://github.com/alvarber/Persimmon',
+      download_url='https://github.com/AlvarBer/Persimmon/archive/v0.7-beta.tar.gz',
       license='MIT',
       packages=find_packages(),
       install_requires=['Kivy', 'scikit-learn', 'pandas', 'numpy',
